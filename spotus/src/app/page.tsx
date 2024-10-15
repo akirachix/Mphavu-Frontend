a1ae053ca7278982c67f45e08b702c5abcd151ff
esther-njoroge <nyokabinjorogeesther@gmail.com>
2024-10-15 21:50:59 +0300

    made changes

--- spotus/src/app/page.tsx
+++ spotus/src/app/page.tsx
@@ -1,11 +1,14 @@
 import Image from "next/image";
 import LandingPage from "./components/LandingPage";
+import Sidebar from "./components/Layout/AgentSidebar";
+import AgentSidebar from "./components/Layout/AgentSidebar";
 
 export default function Home() {
   return (
     <div>
       <main>
       <LandingPage/>
+      <AgentSidebar/>
        
       </main>
     
