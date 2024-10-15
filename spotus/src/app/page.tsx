7f77d2b0b98befc80463e90ccc06cd114df79f59
Teresiawangui <wanguiiteresiaa@gmail.com>
2024-10-15 23:43:22 +0300

    added statistics

--- spotus/src/app/page.tsx
+++ spotus/src/app/page.tsx
@@ -2,14 +2,15 @@ import Image from "next/image";
 import LandingPage from "./components/LandingPage";
 import Sidebar from "./components/Layout/AgentSidebar";
 import AgentSidebar from "./components/Layout/AgentSidebar";
+import StatisticsForm from "./components/statistics/page";
 
 export default function Home() {
   return (
     <div>
       <main>
-      <LandingPage/>
-      <AgentSidebar/>
-       
+    <LandingPage/>
+       <AgentSidebar/> 
+       <StatisticsForm/>
       </main>
     
     </div>
