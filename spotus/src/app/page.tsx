68ed3b7adb426cd43f0bec93ac66630738fe89ca
student <tuukuodorcas@gmail.com>
2024-10-16 13:28:28 +0300

    added navbar

--- spotus/src/app/page.tsx
+++ spotus/src/app/page.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import LandingPage from "./components/LandingPage";
 import Sidebar from "./components/Layout/AgentSidebar";
 import AgentSidebar from "./components/Layout/AgentSidebar";
+import Navbar from "./components/Navbar";
 
 export default function Home() {
   return (
