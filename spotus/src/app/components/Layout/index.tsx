253a53a262e1e30bb20fa6b15fd301e4ba72de2b
Margaret-Nyambura <margaretmacharianyambura@gmail.com>
2024-10-21 18:37:37 +0300

    Added the Admin side bar

---
+++ spotus/src/app/components/Layout/index.tsx
@@ -0,0 +1,16 @@
+"use client";
+import React from 'react';
+import Sidebar from '../AdminSidebar';
+const Layout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+  return (
+    <div className="flex min-h-screen">
+      <aside className="bg-[#0F75BC] w-[20%]">
+        <Sidebar />
+      </aside>
+      <main className="flex-grow p-4 bg-white">
+        {children}
+      </main>
+    </div>
+  );
+};
+export default Layout;
